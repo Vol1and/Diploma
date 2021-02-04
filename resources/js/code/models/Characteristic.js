3840d25d06674852c0ee6608ef3f239f225a0439
Vol1and <vol1and@hotmail.com>
2021-02-04 22:46:53 +0300

    Добавил возможность выбора характеристики в поступлении. Также немного повышено юзабили формы поступления

--- resources/js/code/models/Characteristic.js
+++ resources/js/code/models/Characteristic.js
@@ -1,15 +1,18 @@
 import CharacteristicPrice from "./CharacteristicPrice";
 
 class Characteristic {
-    constructor(id = -1, serial = "",expiry_date = "",characteristic_price = new CharacteristicPrice(), created_at, updated_at, deleted_at) {
+    constructor(id = -1, name = "", serial = "",expiry_date = "",characteristic_price = new CharacteristicPrice(),ware, created_at, updated_at, deleted_at) {
 
         this.id = id;
+        this.name = name;
         this.serial = serial;
         this.expiry_date = expiry_date;
         this.characteristic_price = characteristic_price;
+        this.ware = ware;
         this.created_at = created_at;
         this.updated_at = updated_at;
         this.deleted_at = deleted_at;
+
 
     }
 
